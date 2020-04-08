81b8467eca65058a37a70e71ec539c604b9bfd42
arthurng <arthur.nemi.g@gmail.com>
2020-04-08 20:32:22 -0300

    Desafio-Completo

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import api from './services/api';
+import "./styles.css";
+
+function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get("/repositories").then(response => {
+      setRepositories(response.data)
+    })
+  }, [])
+
+
+  async function handleAddRepository() {
+    api.post("/repositories", {
+      "title": `Desafio ReactJS + ${Date.now()}`,
+      "url": "https://github.com/ArthurGuilarde",
+      "techs": ["Node.js", "Python"]
+    }).then(response => {
+      setRepositories([...repositories, response.data])
+    })
+  }
+
+  async function handleRemoveRepository(id) {
+    api.delete(`/repositories/${id}`).then(() => {
+      setRepositories(repositories.filter(repo => repo.id !== id));
+    })
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {
+          repositories.map((repo => (
+            <li key={repo.id}> {repo.title}
+              <button onClick={() => handleRemoveRepository(repo.id)}>
+                Remover
+                </button>
+            </li>
+          )))
+        }
+      </ul>
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
